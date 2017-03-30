9fa4213b289c2265d84ab177d5bb11366a0098c4
Jakub Trzebiatowski <cubuspl42@gmail.com>
2017-03-30 19:36:19 +0200

    Syntax highlighting

---
+++ src/Lexer.kt
@@ -0,0 +1,151 @@
+fun Char.isLetter(): Boolean {
+    return (this in ('a'..'z')) or (this in ('A'..'Z'))
+}
+
+fun Char.isDigit(): Boolean {
+    return (this in ('0'..'9'))
+}
+
+enum class TokenKind {
+    COMMA,
+    COMMENT,
+    CONST,
+    EOL,
+    IDENT,
+    MNEMONIC,
+    REGISTER,
+    WHITESPACE,
+}
+
+data class Token(val kind: TokenKind, val range: IntRange, val value: Int = -1)
+
+val mnemonics = listOf(
+        "ADD",
+        "SUB",
+        "MOV",
+        "SUBS",
+        "BNE",
+        "B"
+)
+
+val inlineWhitespace = listOf(' ', '\t')
+
+class Lexer(private val input: String) : Iterable<Token> {
+    private var inputOffset = 0
+
+    private fun readChar(): Char? {
+        if (inputOffset < input.size) {
+            return input[inputOffset++]
+        } else return null
+    }
+
+    private fun peekChar(): Char? {
+        if (inputOffset < input.size) {
+            return input[inputOffset]
+        } else return null
+    }
+
+    private fun expectChar(expected: Char) {
+        val actual = readChar()
+        if (actual != expected) {
+            throw Exception()
+        }
+    }
+
+    private fun makeRange(function: () -> Unit): IntRange {
+        val start = inputOffset
+        function()
+        val last = inputOffset - 1
+        return (start..last)
+    }
+
+    private fun readNumber(): Int {
+        var digits = ""
+        while (peekChar()?.isDigit() == true) {
+            digits += readChar()
+        }
+        return digits.toInt()
+    }
+
+    private fun readCharToken(kind: TokenKind, c: Char): Token {
+        expectChar(c)
+        return Token(kind, (inputOffset - 1..inputOffset - 1))
+    }
+
+    private fun readComma() = readCharToken(TokenKind.COMMA, ',')
+
+    private fun readComment(): Token {
+        return Token(TokenKind.COMMENT, makeRange {
+            expectChar(';')
+            while (peekChar() !in listOf('\n', null)) {
+                readChar()
+            }
+        })
+    }
+
+    private fun readConst(): Token {
+        var value = 0
+        return Token(TokenKind.CONST, makeRange {
+            expectChar('#')
+            value = readNumber()
+        }, value)
+    }
+
+    fun readEol() = readCharToken(TokenKind.EOL, '\n')
+
+    private fun readIdentOrMnemonic(): Token {
+        var name = ""
+        val range = makeRange {
+            while (peekChar()?.isLetter() == true) {
+                name += readChar()
+            }
+        }
+        val kind = when {
+            name in mnemonics -> TokenKind.MNEMONIC
+            else -> TokenKind.IDENT
+        }
+        return Token(kind, range)
+    }
+
+    private fun readRegister(): Token {
+        var value = 0
+        return Token(TokenKind.REGISTER, makeRange {
+            val r = readChar()
+            if (r?.toLowerCase() != 'r') {
+                throw Exception()
+            }
+            value = readNumber()
+        }, value)
+    }
+
+    private fun readWhitespace(): Token {
+        return Token(TokenKind.WHITESPACE, makeRange {
+            while (peekChar() in inlineWhitespace) {
+                readChar()
+            }
+        })
+    }
+
+    fun readToken(): Token? {
+        val c = peekChar()
+
+        return when {
+            c == null -> null
+            c.toLowerCase() == 'r' -> readRegister()
+            c == ',' -> readComma()
+            c == ';' -> readComment()
+            c == '#' -> readConst()
+            c == '\n' -> readEol()
+            c.isLetter() -> readIdentOrMnemonic()
+            c in inlineWhitespace -> readWhitespace()
+            else -> throw Exception()
+        }
+    }
+
+    override fun iterator(): Iterator<Token> {
+        return object : Iterator<Token> {
+            override fun hasNext() = peekChar() != null
+            override fun next() = readToken()!!
+        }
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,24 @@
+import org.w3c.dom.Element
+import kotlin.browser.document
+
+private val asmCodeId = "asm-code"
+
 fun main(args: Array<String>) {
-    println("Hello, world!")
+    val input = document.getElementById(asmCodeId)!!.textContent!!.substring(1)
+    val code = document.getElementById("code")!!
+    val lexer = Lexer(input)
+
+    lexer.forEach { token ->
+        val element: Element = when(token.kind) {
+            TokenKind.EOL -> document.createElement("br")
+            else -> {
+                val span = document.createElement("span")
+                val tokenString = input.substring(token.range).replace(" ", "\u00A0")
+                span.textContent = tokenString
+                span.className = token.kind.toString()
+                span
+            }
+        }
+        code.appendChild(element)
+    }
 }
